e214edb9a8292043e2cb2d722f6d5975c3cc6a10
keaper7 <keaper.sem@gmail.com>
2026-06-30 15:25:57 +0300

    feat: изучено затенение переменных (shadowing)

---
+++ 01-scope/scope.js
@@ -0,0 +1,8 @@
+const hero = 'Стажер';
+
+if (true) {
+    const hero = 'Сеньор';
+    console.log('Внутри блока:', hero); // Что выведет здесь?
+}
+
+console.log('Снаружи блока:', hero); // А что выведет здесь?
